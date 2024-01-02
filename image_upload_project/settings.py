4ab59c2ef0d5320e469e1d4960ed89056f630950
vishesh-soni <b20025@students.iitmandi.ac.in>
2024-01-02 21:24:50 +0530

    Adding Files

--- image_upload_project/settings.py
+++ image_upload_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-icdf&88m0nrw!p^ef!0-!flo0nlaz!o2%e+)*8eysgswesl-ko
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
 
 # Application definition
@@ -122,6 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
